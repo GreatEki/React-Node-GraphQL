d942f09eb73a29ec0b71f5d4d9ecdf90dcce776c
Greateki <ekeneidiagbor@gmail.com>
2021-06-07 11:26:47 +0100

    implemented handleChange in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 function App() {
+	const [name, setName] = useState('');
+	const [userName, setUserName] = useState('');
+	const [password, setPassword] = useState('');
+
 	const client = new ApolloClient({
 		uri: 'http://localhost:5000/graphql',
 		cache: new InMemoryCache(),
@@ -12,6 +16,32 @@ function App() {
 	return (
 		<ApolloProvider client={client}>
 			<div> The GraphQL Application</div>
+
+			<div className='createUser'>
+				<div>
+					<label> Name </label>
+					<br />
+					<input type='text' onChange={(e) => setName(e.currentTarget.value)} />
+				</div>
+				<div>
+					<label> Username </label>
+					<br />
+					<input
+						type='text'
+						onChange={(e) => setUserName(e.currentTarget.value)}
+					/>
+				</div>
+				<div>
+					<label> Password </label>
+					<br />
+					<input
+						type='text'
+						onChange={(e) => setPassword(e.currentTarget.value)}
+					/>
+				</div>
+
+				<button>Create User</button>
+			</div>
 		</ApolloProvider>
 	);
 }
